572852a72a57517def51a2def511b110a1ce1b8f
Jeremy Green <jeremy@octolabs.com>
2013-06-17 20:41:15 -0500

    3.  Add a simple model

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
 UselessApp = Ember.Application.create();
 
+UselessApp.Person = Ember.Object.extend({
+  firstName: null,
+  lastName: null,
+
+  fullName: function() {
+    return this.get('firstName') + " " + this.get('lastName');
+  }.property('firstName', 'lastName')
+});
